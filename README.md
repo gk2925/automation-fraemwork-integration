9b4416e00e2f06322a9aa74a013c9027930c1960
gaurav.tyagi <gaurav.tyagi@calrivate.com>
2023-03-04 16:22:34 +0100

    making some changes in README.md

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # JUnit5 sample project
 
-## How to use the project
+## Pre-requisites for running the tests in the project
+
+- Java must be installed on your system (JDK8 or higher)
+- Maven must be installed on your system
+- Python3 must be installed on your system
+- Chrome browser must be installed on the system for running the tests (109 or higher)
+
+## How to parse the test results to testrail without using any CI/CD tool
 
 - Replace the placeholders in `trcli-config.yml` with your TestRail instance details
-- Execute the commands on the script below
+- Execute the commands on the script below if you would like to parse test results without any CI/CD tool
 
 ```sh
 # Install TR CLI
@@ -13,9 +20,9 @@ pip install trcli
 mvn clean compile
 
 # Run tests
-mvn mvn clean compile test
+mvn clean compile test
 
-# Upload test results
+# Upload test results to Testrail
 trcli -y -c "trcli-config.yml" parse_junit -f "./target/TEST-junit-jupiter.xml"
 
 ```
--- src/test/java/com/idera/testrail/tests/HomePageTest.java
+++ src/test/java/com/idera/testrail/tests/HomePageTest.java
@@ -1,33 +1,39 @@
 package com.idera.testrail.tests;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-class MultiplicationTests {
+class HomePageTest {
 
-    @Test
-    void MultiplyTwoNumbers() {
+    public static WebDriver driver;
 
+    @BeforeEach
+    public void setUp() {
         String os = SystemUtils.OS_NAME;
         System.out.println("The name of the operating system is " + os);
         String pathToChromeDriver = null;
-
         if (os.contains("Windows")) {
             pathToChromeDriver = "drivers/windows/chromedriver.exe";
         } else {
             pathToChromeDriver = "drivers/linux/chromedriver";
         }
-
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("--window-size=1920,1080");
@@ -41,19 +47,32 @@ class MultiplicationTests {
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.setExperimentalOption("useAutomationExtension", false);
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-
-        //Step 2- Navigation: Open a website
-        driver.navigate().to("https://www.swtestacademy.com/");
-        //Step 3- Assertion: Check its title is correct
-        //assertEquals method Parameters: Expected Value, Actual Value, Assertion Message
-        assertEquals("Software Test Academy", driver.getTitle(), "Title check failed!");
-        //Step 4- Close Driver
-        driver.close();
-        //Step 5- Quit Driver
-        driver.quit();
-
+        //Navigation: Open a website
+        driver.navigate().to("https://www.gurock.com/testrail");
     }
+
+    @AfterEach
+    public void tearDown(){
+        //Quit Driver after completing of every test
+        driver.quit();
+    }
+
+    @Test
+    void verifyTitleOfHomePage() {
+        //Assertion: Check its title is correct
+        //assertEquals method Parameters: Expected Value, Actual Value, Assertion Message
+        assertEquals("TestRail: Test Management & QA Software for Agile Teams", driver.getTitle(), "Title check failed!");
+    }
+
+    @Test
+    void verifyPresenceOfDemoLinkOnHomePage() {
+        //Assertion: Check the presence of demo link
+        WebElement demoLinkElement = driver.findElement(By.cssSelector("a.btn.btn-medium.btn-outline-white"));
+        assertTrue(demoLinkElement.isDisplayed());
+    }
+
+
 }
